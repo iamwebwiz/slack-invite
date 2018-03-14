32e809d72622c5d35692f537aa1340f9fa3097a9
iamwebwiz <iamwebwiz@gmail.com>
2018-03-14 02:29:37 +0100

    LaravelSlack package included, GuzzleHttp client included

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/slack', [
+    'uses'=>'LaravelSlackController@slackPage',
+    'as'=>'slack'
+]);
+
+Route::post('/slack', [
+    'uses'=>'LaravelSlackController@sendSlackInvite',
+    'as'=>'slack'
+]);
