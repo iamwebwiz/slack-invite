7ff532ad0daeae450b8c1366533e2cef169d0ce6
Ezekiel <iamwebwiz@gmail.com>
2020-09-22 14:08:08 +0100

    update slack controller

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Exception;
 use GuzzleHttp\Client as GuzzleClient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class SlackController extends Controller
 {
@@ -24,26 +26,29 @@ class SlackController extends Controller
 
     public function sendInvitation(Request $request)
     {
-        $rules = ['email' => 'required|string|email'];
-        $validator = validator($request->all(), $rules);
+        $validation = $this->validation($request->all());
+
         $email = $request->input('email');
 
-        if ($validator->fails()) {
-            return redirect()->back()->with('error', 'You must enter your email to proceed.');
+        if ($validation->fails()) {
+            return back()->with('error', 'You must enter your email to proceed.');
         } else {
             try {
-                $this->client->request(
-                    'POST',
-                    config('sitedata.slack_team_url') . '/api/users.admin.invite?t='
-                        . time() . '&email=' . $email . '&token=' . config('sitedata.slack_api_token')
-                        . '&set_active=true&_attempts=1'
-                );
+                $url = config('sitedata.slack_team_url') . '/api/users.admin.invite?t=' . time() . "&email={$email}";
+                $url .= '&token=' . config('sitedata.slack_api.token') . '&set_active=true&_attempts=1';
 
-                return redirect()->back()->with('success', "An invitation to your mail to join {$this->teamName} workspace.");
-            } catch (Exception $e) {
-                // abort(404);
-                return redirect()->back()->with('error', 'An error occured while sending invitation, please try again.');
+                $this->client->request('POST', $url);
+
+                return back()->with('success', "An invitation to your mail to join {$this->teamName} workspace.");
+            } catch (Exception $exception) {
+                Log::error($exception->getMessage());
+                return back()->with('error', 'An error occured while sending invitation, please try again.');
             }
         }
+    }
+
+    private function validation($inputs)
+    {
+        return Validator::make($inputs, ['email' => 'required|string|email|max:191']);
     }
 }
